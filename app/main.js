c5447f266ab831b3151f7a7eb116a71dc1be122a
Louis Simons <lousimons@gmail.com>
2012-02-18 21:40:09 -0500

    Incorporating tbranyen's updates

---
+++ app/main.js
@@ -0,0 +1,90 @@
+require([
+  "namespace",
+
+  // Libs
+  "jquery",
+  "use!backbone",
+  "io",
+
+  // Modules
+  "modules/home",
+  "modules/user"
+],
+
+function(namespace, jQuery, Backbone, Io, Home, User) {
+  
+  // Defining the application router, you can attach sub routers here.
+  var Router = Backbone.Router.extend({
+    routes: {
+      "": "index",
+      "logout": "logout"
+    },
+
+    index: function() {
+      var route = this;
+
+      $.get('/session/user_id', function(data) {
+        if (data.user_id) {
+          
+          var user = new User.Model({_id: data.user_id});
+          user.fetch();
+
+          var home = new Home.Model({user: user});
+          var home_view = new Home.Views.Main({model: home});         
+
+        } else {
+
+          // Get /session/new then redirect back
+          $.get('/session/new', function(data) {
+            window.location = '/';
+          });
+
+        }
+      });
+    },
+
+    logout: function() {
+      $.get('/session/destroy', function(data) {
+            window.location = '/';
+      });
+    }
+  });
+
+  // Treat the jQuery ready function as the entry point to the application.
+  // Inside this function, kick-off all initialization, everything up to this
+  // point should be definitions.
+  jQuery(function($) {
+    // Shorthand the application namespace
+    var app = namespace.app;
+    
+    // Define your master router on the application namespace and trigger all
+    // navigation from this instance.
+    app.router = new Router();
+
+    // Trigger the initial route and enable HTML5 History API support
+    Backbone.history.start({ pushState: true });
+
+    // All navigation that is relative should be passed through the navigate
+    // method, to be processed by the router.  If the link has a data-bypass
+    // attribute, bypass the delegation completely.
+    $(document).on("click", "a:not([data-bypass])", function(evt) {
+      // Get the anchor href and protcol
+      var href = $(this).attr("href");
+      var protocol = this.protocol + "//";
+
+      // Ensure the protocol is not part of URL, meaning its relative.
+      if (href && href.slice(0, protocol.length) !== protocol) {
+        // Stop the default event to ensure the link will not cause a page
+        // refresh.
+        evt.preventDefault();
+
+        // This uses the default router defined above, and not any routers
+        // that may be placed in modules.  To have this work globally (at the
+        // cost of losing all route events) you can change the following line
+        // to: Backbone.history.navigate(href, true);
+        app.router.navigate(href, true);
+      }
+    });
+  });
+  
+});
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@ Yet Another Chat App
 
 This is based on tbranyen's excellent [boiler-plate](https://github.com/tbranyen/backbone-boilerplate/tree/amd) for NodeJS/Backbone projects.  I suggest everybody use that so I can have more people to ask questions of.
 
-Last updated to "moved scripts to..." from 2/9/12, bfa522b9c70b46e62aa405676f76a236caa4c8d1
+Last updated to "broke main application code out..." from 2/13/12, c702081c87728f4275aa0fb074129fd997c9bff9
 
 If someone knows a better way to merge in changes from another git project that's not really a fork, please let me know.
 
